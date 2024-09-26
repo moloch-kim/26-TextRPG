cf0c15d43eb57ed51e40e2b27576d276f6733631
moloch-kim <79761359+moloch-kim@users.noreply.github.com>
2024-09-26 11:01:16 +0900

    Create Main Solution

---
+++ 26조TextRPG/26조TextRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _26조TextRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
